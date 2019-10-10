6c7a5a67771def2e2dac08029b1e1a1c87a5f8b9
Minh Lam <mlam@oberlin.edu>
2019-10-10 19:30:01 -0400

    More Updates

---
+++ week-6-codealong/week-6.js
@@ -0,0 +1,56 @@
+// Defining prices for different items
+var priceList = {
+    piano: 700,
+    book: 2000,
+    bottle: 1,
+    propaganda: 10,
+}
+
+/* Change code below this line */
+function getNewTotal(currentTotal, newItem) {
+  var price = priceList[newItem];
+  return currentTotal + price;
+}
+
+/* Change code above this line */
+
+
+function addItem() {
+  /* Grab element from html */
+  var itemName = document.getElementById('item-input').value;
+  itemName.trim();
+  itemName = itemName.replace(' poster', '');
+  if (!priceList.hasOwnProperty(itemName)) {
+    return;
+  }
+  var total = document.getElementById('total');
+  total.innerText = getNewTotal(parseFloat(total.innerText || 0), itemName);
+
+  var item = document.getElementById(itemName);
+
+  var shoppingList = document.getElementById('shopping-list');
+  if (!item) {
+    shoppingList.innerHTML += '<div id="' + itemName + '">'
+                            + '<img class="remove-button" src="./remove-item.png" id="remove-' + itemName
+                            + '" onclick="removeItem(' + itemName.toString() + ", " + parseFloat(total.innerText || 0) + ')"> '
+                            + (itemName.replace('propaganda', 'propaganda poster'))
+                            + '</div>'
+  }
+  else if (item.style.display === "none") {
+    item.style.display = "block";
+  }
+}
+
+function removeItem(itemName, currentTotal) {
+  var item = document.getElementById(itemName.id);
+  if (item.style.display === "none") {
+    item.style.display = "block";
+  } else {
+    item.style.display = "none";
+  }
+
+  var total = document.getElementById('total');
+  total.innerText = getReducedTotal(parseFloat(total.innerText || 0), itemName.id);
+}
+
+
