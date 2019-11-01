852f0763cd65faa970e19d7fa6a9056fcf91b518
Minh Lam <mlam@oberlin.edu>
2019-10-31 21:30:09 -0400

    End of 10/31/2019 lesson

---
+++ week-8/webco-shoppe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// App is a component
+class App extends React.Component {
+  render() {
+    return <h1>WebCo Shoppe</h1>;
+    // <Description />
+    // <Cart />);
+  }
+}
+
+export default App;
