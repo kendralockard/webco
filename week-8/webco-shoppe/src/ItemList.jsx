cf11dd3387d3864de9c7c5189ac3ce37ff5e6929
Minh Lam <mlam@oberlin.edu>
2019-11-06 18:50:16 -0500

    wip

--- week-8/webco-shoppe/src/ItemList.jsx
+++ week-8/webco-shoppe/src/ItemList.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class ItemList extends React.Component {
   render() {
-    const items = this.props.items.map(str => <li>{str}</li>);
+    const items = this.props.items.map(str => (
+      <div className="header item">
+        <li>{str}</li>
+      </div>
+    ));
     return <ul>{items}</ul>;
   }
 }
